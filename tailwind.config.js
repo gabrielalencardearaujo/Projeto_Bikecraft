7cc625b6d6ea020ed6c963047d24e03afb4f3f76
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-09-13 18:06:58 -0300

    update responsive page home

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,6 +40,22 @@ module.exports = {
     },
 
     extend: {},
+    screens: {
+      'xs': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '840px',
+      // => @media (min-width: 840px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 }
